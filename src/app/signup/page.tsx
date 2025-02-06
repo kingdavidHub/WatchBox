886195f944498d4aa4b0475bc234c4d9939da414
David Igbayilola <kingigbayilola@gmail.com>
2025-02-06 09:18:03 +0100

    feat: Update SignUp page layout with background image and form structure

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,13 @@
-import endlessConstellation from "../../assets/images/endless-constellation.png";
-
 const SignUp = () => {
   return (
-    <div className={`min-h-screen bg-[${endlessConstellation}]`}>
-      <main>
-        <h1 className="text-white">signup</h1>
-      </main>
+    <div className="min-h-screen bg-[url(@/assets/images/endless-constellation.png)] bg-center bg-contain bg-gray-900 flex justify-center items-center">
+        <div className="bg-white shadow-md p-2">
+          <form action="">
+          <h1>Hello world</h1>
+          </form>
+        </div>
     </div>
   );
 };
-
 
 export default SignUp;
